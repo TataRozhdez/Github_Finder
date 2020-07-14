3bdb229d4cb5b0f67e2772cafd5a409d544b5cae
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2020-07-14 18:19:02 +0300

    alert

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react'
+import GithubContext from '../../context/github/githubContext'
+import AlertContext from '../../context/alert/alertContext'
 
-const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
+const Search = () => {
+  const githubContext = useContext(GithubContext)
+  const alertContext = useContext(AlertContext)
+
   const [text, setText] = useState('')
 
   const onChange = (e) => setText(e.target.value)
@@ -10,9 +14,9 @@ const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
     e.preventDefault()
 
     if (text === '') {
-      setAlert('Please enter something', 'Light');
+      alertContext.setAlert('Please enter something', 'Light');
     } else {
-      searchUsers(text)
+      githubContext.searchUsers(text)
       setText('')
     }
   }
@@ -33,20 +37,13 @@ const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
           className='btn btn-dark btn-block'
         />
       </form>
-      {showClear && (
-        <button className='btn btn-light btn-block' onClick={clearUsers}>
+      {githubContext.users.length > 0 && (
+        <button className='btn btn-light btn-block' onClick={githubContext.clearUsers}>
           Clear
         </button>
       )}
     </div>
   )
-}
-
-Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired,
 }
 
 export default Search
